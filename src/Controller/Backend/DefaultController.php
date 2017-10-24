8a0d880e3d2cac11b1715f1c508af9824b2ba71c
FishmanDev <fishman.code@gmail.com>
2017-10-24 13:08:43 +0300

    Create 'login' and 'logout' actions

--- src/Controller/Backend/DefaultController.php
+++ src/Controller/Backend/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Backend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class DefaultController extends Controller
@@ -24,5 +25,30 @@ class DefaultController extends Controller
     public function adminAction()
     {
         return $this->render('backend/default/admin.html.twig');
+    }
+
+    /**
+     * @Route("/admin/login", name="login")
+     * @param AuthenticationUtils $authUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction(AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+        return $this->render('backend/default/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/admin/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+        // Does not need to be implemented
     }
 }
